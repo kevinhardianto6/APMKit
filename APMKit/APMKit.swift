6edf80a0c8082f2431930134f9a60860e44da069
Kevin Hardianto <hardiantokevin00@gmail.com>
2026-08-24 11:17:35 +0700

    Initial Commit

---
+++ APMKit/APMKit.swift
@@ -0,0 +1,9 @@
+//
+//  APMKit.swift
+//  APMKit
+//
+//  Created by Kevin Hardianto on 24/08/26.
+//
+
+import Foundation
+
---
+++ APMKitTests/APMKitTests.swift
@@ -0,0 +1,19 @@
+//
+//  APMKitTests.swift
+//  APMKitTests
+//
+//  Created by Kevin Hardianto on 24/08/26.
+//
+
+import Testing
+@testable import APMKit
+
+struct APMKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
